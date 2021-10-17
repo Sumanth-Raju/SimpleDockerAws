fac01e43ed5139225d52c5a3a51de7d332347cda
Sumanth Gowda <mailtosumanthgowdar@gmail.com>
2021-10-18 00:52:06 +0530

    initial commit to feature

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hi Sumanth how are you doing now</p>
+        <p>Hi Sumanth how are you doing now in feature</p>
         <a
           className="App-link"
           href="https://reactjs.org"
